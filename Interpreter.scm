63cb7b351a298b4408dde684708f15a8214c7384
jihandong <jihandong@xiaomi.com>
2021-10-29 20:37:28 +0800

    add some helpter functions to Interpreter.scm

--- Interpreter.scm
+++ Interpreter.scm
@@ -1,5 +1,6 @@
 #lang scheme
 
+;; evaluating expression.
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
@@ -9,8 +10,8 @@
         ((if? exp) (eval-if exp env))
         ((lambda? exp)
          (make-procedure (lambda-parameters exp)
-                                       (lambda-body exp)
-                                       env))
+                         (lambda-body exp)
+                         env))
         ((begin? exp)
          (eval-sequence (begin-actions exp) env))
         ((cond? exp) (eval (cond->if exp) env))
@@ -20,6 +21,7 @@
         (else
          (error "Error in eval: unkown expression type" exp))))
 
+;; applying procedure.
 (define (apply procedure arguments)
   (cond ((primitive-procedure? procedure)
          (apply-primitive-procedure procedure arguments))
@@ -31,3 +33,114 @@
                          (procedure-environment procedure))))
         (else
          (error "Error in apply: unkown procedure type" exp))))
+
+(define (list-of-value exps env)
+  (if (null? exps)
+      '()
+      (cons (eval (car exps) env)
+            (list-of-value (cdr exps) env))))
+
+;; self-evaluating expression.
+(define (self-evaluating? exp)
+  (cond ((number? exp) #t)
+        ((string? exp) #t)
+        (else #f)))
+
+;; variable expression.
+(define (variable? exp) (symbol? exp))
+
+;; quote expression.
+(define (quoted? exp) (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pairs? exp)
+      (eq? (car exp) tag)
+      #f))
+
+;; assignment expression.
+(define (eval-assignment exp env)
+  (set-variable! (asssignment-variable exp)
+                 (eval (assignment-value exp) env)
+                 env)
+  'ok)
+
+(define (assignment? exp) (tagged-list? exp 'set!))
+
+(define (assignment-variable exp) (cadr exp))
+
+(define (assignment-value exp) (caddr exp))
+
+;; definition expression.
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'ok)
+
+(define (definition? exp) (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (car exp))
+      (cadr exp)
+      (caddr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)
+                   (cddr exp))))
+
+;; lambda expression
+(define (lambda? exp) (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp) (cadr exp))
+
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda parameters body)
+  (list 'lambda parameters body))
+
+;; if expression.
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define (if? exp) (tagged-list? exp 'if))
+
+(define (if-predicate exp) (cadr exp))
+
+(define (if-consequent exp) (caddr exp))
+
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp))) ;else part can be empty.
+      (cadddr exp)
+      #f))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative)
+
+;; begin expression (sequence).
+(define (eval-sequence exps env)
+  (if (last-exps? exps) ;return value of last expression.
+      (eval (first-exp exps) env)
+      (eval (first-exp exps) env) (eval-sequence (rest-exps exps) env)))  
+  
+(define (begin? exp) (tagged-list? exp 'begin))
+
+(define (begin-actions exp) (cdr exp))
+
+(define (last-exps? seq) (null? (cdr exp)))
+
+(define (first-exp seq) (car seq))
+
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exps? seq) (first-exp seq))
+        (else (make-begin seq))))
+
+(define (make-begin seq) (cons begin seq))
